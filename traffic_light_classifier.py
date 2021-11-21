ab3e7ef7658ef39d090b1b397e98260172b00e36
RabebMhamdi <mhaamdi.rabeb@gmail.com>
2021-11-21 17:40:58 +0100

    Update traffic_light_classifier.py

--- traffic_light_classifier.py
+++ traffic_light_classifier.py
@@ -1,16 +1,14 @@
 import cv2 # computer vision library
+
 import helpers # helper functions
+import test_functions
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg # for loading in images
 
-import test_functions
 %matplotlib inline
-
-
-
 
 # This function should take in an RGB image and return a new, standardized version
 def standardize_input(image):   
@@ -106,13 +104,6 @@ def get_misclassified_images(test_images):
     return misclassified_images_labels
 
 
-
-# Image data directories
-IMAGE_DIR_TRAINING = "traffic_light_images/training/"
-IMAGE_DIR_TEST = "traffic_light_images/test/"
-# Using the load_dataset function in helpers.py
-# Load training data
-IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TRAINING)
 
 # Image data directories
 IMAGE_DIR_TEST = "traffic_light_images/test/"
