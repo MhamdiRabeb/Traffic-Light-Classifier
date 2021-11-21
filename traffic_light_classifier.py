257fb9541142b235a79c1dfe1362b9167d2a3ac5
RabebMhamdi <mhaamdi.rabeb@gmail.com>
2021-11-21 19:14:44 +0100

    Update traffic_light_classifier.py

--- traffic_light_classifier.py
+++ traffic_light_classifier.py
@@ -1,26 +1,60 @@
 import cv2 # computer vision library
 
 import helpers # helper functions
-import test_functions
+import test_functions # test functions
 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg # for loading in images
 
-%matplotlib inline
 
-# This function should take in an RGB image and return a new, standardized version
-def standardize_input(image):   
-    ## TODO: Resize image and pre-process so that all "standard" images are the same size  
-    standard_im = np.copy(image) 
+
+def vizualize_input(image_list):
+    yellow_index=730
+    red_index=0
+    green_index=783
+
+    selected_image_red = IMAGE_LIST[red_index][0]
+    selected_label_red = IMAGE_LIST[red_index][1]
+
+    selected_image_yellow = IMAGE_LIST[yellow_index][0]
+    selected_label_yellow = IMAGE_LIST[yellow_index][1]
+
+    selected_image_green = IMAGE_LIST[green_index][0]
+    selected_label_green = IMAGE_LIST[green_index][1]
+
+    # Visualize the individual color channels
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3)
+    
+    ax1.set_title('red light')
+    print('Image shape: ', selected_image_red.shape)
+    print('Image label: ', selected_label_red)
+    ax1.imshow(selected_image_red)
+
+    ax2.set_title('yellow light')
+    print('Image shape: ', selected_image_yellow.shape)
+    print('Image label: ', selected_label_yellow)
+    ax2.imshow(selected_image_yellow)
+
+    ax3.set_title('green light')
+    print('Image shape: ', selected_image_green.shape)
+    print('Image label: ', selected_label_green)
+    ax3.imshow(selected_image_green)
+    
+    return True
+    
+def standardize_input(image):
+    """ This function takes in an RGB image and return a new, standardized version 
+        by resizing the image so that all "standard" images are the same size (32x32) 
+    """    
+    standard_image = np.copy(image) 
     # Use OpenCV's resize function
-    standardized_im = cv2.resize(standard_im, (32, 32))    
-    return standardized_im
+    standard_image = cv2.resize(standard_im, (32, 32))    
+    return standard_image
 
-## TODO: One hot encode an image label
-## Given a label - "red", "green", or "yellow" - return a one-hot encoded label
-def one_hot_encode(label):    
+def one_hot_encode(label): 
+    " Given a label - red, green, or yellow - return a one-hot encoded label"
     dic_standard={'red':[1,0,0],'yellow':[0,1,0],'green':[0,0,1]}
     one_hot_encoded=dic_standard[label]
     return one_hot_encoded
@@ -36,18 +70,18 @@ def standardize(image_list):
         standardized_im = standardize_input(image)
         # One-hot encode the label
         one_hot_label = one_hot_encode(label)    
-        # Append the image, and it's one hot encoded label to the full, processed list of image data 
+        # Append the image and it's one hot encoded label to the full, processed list of image data 
         standard_list.append((standardized_im, one_hot_label))        
     return standard_list
 
-## TODO: Create a brightness feature that takes in an RGB image and outputs a feature vector and/or value
-## This feature should use HSV colorspace values
+
 def create_feature(rgb_image):
+    """ Create a brightness feature that takes in an RGB image and outputs a feature vector
+        using HSV colorspace values """
     
-    #convert your image to HSV
-    ## TODO: Convert image to HSV color space
+    # Convert image to HSV color space
     hsv = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)   
-    ## TODO: Create and return a feature value and/or vector
+    # Create and return a feature vector
     feature = []
     #keep only the v-channel
     v = hsv[:,:,2]    
@@ -60,50 +94,56 @@ def create_feature(rgb_image):
     avg_brightness = [np.average(region) for region in regions]
     return avg_brightness
   
-  def improve_feature(rgb_image):
+def improve_feature(rgb_image):
+    " to help accurately label the traffic light images "
     feature=create_feature(rgb_image)
     maxBrightness=np.argmax(feature)
     return maxBrightness  
   
-# This function should take in RGB image input
-# Analyze that image using your feature creation code and output a one-hot encoded label
+
 def estimate_label(rgb_image):   
-    ## TODO: Extract feature(s) from the RGB image and use those features to
-    ## classify the image and output a one-hot encoded label
+    """ This function takes in an RGB image as input, analyze that image 
+    using the feature creation code and output a one-hot encoded label """   
     predicted_label = []
+    #Extract feature(s) from the RGB image
     maxBrightness=improve_feature(rgb_image)
     if maxBrightness==0:
         label="red"
     elif maxBrightness==1:
         label="yellow"
     elif maxBrightness==2:
-        label="green"
-    predicted_label_ohl=one_hot_encode(label)    
+        label="green"   
+    #output a one-hot encoded label
+    predicted_label_ohl=one_hot_encode(label)       
     return predicted_label_ohl
          
-# Constructs a list of misclassified images given a list of test images and their labels
-# This will throw an AssertionError if labels are not standardized (one-hot encoded)
 def get_misclassified_images(test_images):
+    " Constructs a list of misclassified images given a list of test images and their labels "
+    
     # Track misclassified images by placing them into a list
     misclassified_images_labels = []
     # Iterate through all the test images
     # Classify each image and compare to the true label
-    for image in test_images:
+    for test_image in test_images:
         # Get true data
-        im = image[0]
-        true_label = image[1]
+        image = test_image[0]
+        true_label = test_image[1]
         assert(len(true_label) == 3), "The true_label is not the expected length (3)."       
         # Get predicted label from your classifier
-        predicted_label = estimate_label(im)
+        predicted_label = estimate_label(image)
         assert(len(predicted_label) == 3), "The predicted_label is not the expected length (3)."
         # Compare true and predicted labels 
         if(predicted_label != true_label):
             # If these labels are not equal, the image has been misclassified
-            misclassified_images_labels.append((im, predicted_label, true_label))            
+            misclassified_images_labels.append((image, predicted_label, true_label))            
     # Return the list of misclassified [image, predicted_label, true_label] values
     return misclassified_images_labels
 
-
+# Load training data
+IMAGE_DIR_TRAINING = "traffic_light_images/training/"
+IMAGE_LIST = helpers.load_dataset(IMAGE_DIR_TRAINING)
+# Display the image, shape and label
+vizualize_input(IMAGE_LIST)
 
 # Image data directories
 IMAGE_DIR_TEST = "traffic_light_images/test/"
@@ -114,17 +154,24 @@ STANDARDIZED_TEST_LIST = standardize(TEST_IMAGE_LIST)
 # Find all misclassified images in a given test set
 MISCLASSIFIED = get_misclassified_images(STANDARDIZED_TEST_LIST)
 
+# Check criteria 1
 # Accuracy calculations
 total = len(STANDARDIZED_TEST_LIST)
 num_correct = total - len(MISCLASSIFIED)
 accuracy = num_correct/total
-
 print('Accuracy: ' + str(accuracy))
 print("Number of misclassified images = " + str(len(MISCLASSIFIED)) +' out of '+ str(total))
-# Visualize misclassified example(s)
-## TODO: Display an image in the `MISCLASSIFIED` list 
+# Display an image in the `MISCLASSIFIED` list 
 misclassified_image=MISCLASSIFIED[13][0]
 plt.imshow(misclassified_image)
-## TODO: Print out its predicted label - to see what the image *was* incorrectly classified as
+# Print out its predicted label - to see what the image *was* incorrectly classified as
 misclassified_image_label=MISCLASSIFIED[0][1]
 print("misclassified_image_label", misclassified_image_label)
+
+# Check criteria 2
+tests = test_functions.Tests()
+if(len(MISCLASSIFIED) > 0):
+    # Test code for one_hot_encode function
+    tests.test_red_as_green(MISCLASSIFIED)
+else:
+    print("MISCLASSIFIED may not have been populated with images.")
